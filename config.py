6044b76ad30cab2cab77e6418225bcf02893e411
Джейсон <artoreshin@gmail.com>
2020-04-21 00:07:46 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+chats = ["unode_net_chat","reclamma777","lovecoin_1","SpaceJackpotChat"]
+api_id = NO
+api_hash = "NO"
---
+++ dicetimefucker.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+import config
+import time
+api_id = config.api_id
+api_hash = config.api_hash
+
+count_accounts = int(input("Input Counts Accounts: "))
+
+
+for i in range(count_accounts):
+    with Client("my_account" + str(i), api_id, api_hash) as app:
+        for i in config.chats:
+            app.join_chat(i)
+            time.sleep(3)
+            app.send_message(i, "dice time")
+            time.sleep(3)
